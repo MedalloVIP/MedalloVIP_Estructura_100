c2798750658d2e82328e3a970d073c44dc91b7c1
MedalloVIP <marketing@medallovip.live>
2025-05-09 20:14:53 -0500

    Update transmision-control.js

--- js/transmision-control.js
+++ js/transmision-control.js
@@ -2,3 +2,9 @@ import { inicializarControlMicrofono } from "./audio-control.js";
 
 // Después de crear los tracks y adjuntar el video
 inicializarControlMicrofono(audioTrack, btnMic);// Código principal de transmisión
+
+
+import { iniciarMedicionVelocidad } from "./velocidad-control.js";
+
+const velocidadEl = document.getElementById("velocidad");
+iniciarMedicionVelocidad(velocidadEl);
