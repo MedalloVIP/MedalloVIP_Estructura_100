7e1f3cad9040b4efa0656bfbabcb2b656495692d
MedalloVIP <marketing@medallovip.live>
2025-05-15 12:26:27 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7pcneppnTngr3et84W_rXcQ6Q6DKTIU4",
+  apiKey: "AIzaSyD7pcnepnpTngr3et84W_rXcQ6GQDKTlU4",
   authDomain: "medallovip-bae9f.firebaseapp.com",
   projectId: "medallovip-bae9f",
   storageBucket: "medallovip-bae9f.appspot.com",
   messagingSenderId: "97246531560",
-  appId: "1:97246531560:web:64be84a8e9dece26741e5d",
-  measurementId: "G-BCLX5482R3"
+  appId: "1:97246531560:web:64be84a8c9dece26741e5d",
+  measurementId: "G-BCLX842R83"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export { app };
