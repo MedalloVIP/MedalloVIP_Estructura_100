e7164c9169a5d0d27b7e7140d0f4e8a18092d397
MedalloVIP <marketing@medallovip.live>
2025-05-15 12:16:44 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,16 @@
 // firebase-config.js
-
-// Importar la función de inicialización desde Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-// Configuración oficial de Firebase para MedalloVIP
 const firebaseConfig = {
-  apiKey: "AIzaSyC5GDaZOlXK8xZKgr2wShlQ1_fh6k6O-0g",
-  authDomain: "medallovip.firebaseapp.com",
-  projectId: "medallovip",
-  storageBucket: "medallovip.appspot.com",
-  messagingSenderId: "1032680188247",
-  appId: "1:1032680188247:web:aa3fdd233a4a8d9998cdbe",
-  measurementId: "G-GMKVZ58W07",
-  databaseURL: "https://medallovip-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyD7pcneppnTngr3et84W_rXcQ6Q6DKTIU4",
+  authDomain: "medallovip-bae9f.firebaseapp.com",
+  projectId: "medallovip-bae9f",
+  storageBucket: "medallovip-bae9f.appspot.com",
+  messagingSenderId: "97246531560",
+  appId: "1:97246531560:web:64be84a8e9dece26741e5d",
+  measurementId: "G-BCLX5482R3"
 };
 
-// Inicializar la app de Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar la instancia para usar en otros módulos (como login-control.js o registro-control.js)
 export { app };
